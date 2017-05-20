1d2075aa05f80e7683044f71fd8bf21dd77546ed
Jorgesanel <jorgesantosneila@hotmail.com>
2017-05-20 18:36:01 +0100

    Get questions from DB

--- imports/api/users.js
+++
@@ -1,4 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const Questions = new Mongo.Collection('Questions');
-
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,6 +2,13 @@ import { Template } from 'meteor/templating';
 import { menu } from './structure/menu.js';
 import { footer } from './structure/footer.html';
 import { userLogIn } from './userLogIn.js'
+import { Questions } from '../api/questions.js';
 
 import './body.html';
 import './body.css';
+
+Template.body.helpers({
+    Questions(){
+         return Questions.find()
+    }
+})
