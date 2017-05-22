94b9e4f75821640d04b5033fbb5b17935bf07439
Jorgesanel <jorgesantosneila@hotmail.com>
2017-05-22 22:51:30 +0100

    Read Questions from DataBase and Check if the user's answer is the correct one

--- imports/api/questions.js
+++ imports/api/questions.js
@@ -2,5 +2,23 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Questions = new Mongo.Collection('questions');
-	
+
+Meteor.methods({
+    'questions.checkAnswer'(answer_User, idQuestion) {
+Console.Log("Entramos");
+        // Make sure the user is logged in before inserting a task
+        if (!Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+        var pregunta = Questions.findOne({ "_id": idQuestion });
+
+        if (pregunta.correctAnswer == answer_User) {
+            Console.Log("Pregunta Acertada");
+        }
+        else {
+            Console.Log("Pregunta Fallada");
+        }
+    },
+});
+
 
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -8,7 +8,30 @@ import './body.html';
 import './body.css';
 
 Template.body.helpers({
-    Questions(){
-         return Questions.find()
-    }
+    Questions() {
+        return Questions.find()
+    },
 })
+
+Template.question.events({
+    'click :button': function (event, template) {
+        var idQuestion = 1;
+        var answer = template.find('input:radio[name=answer]:checked');
+        var userAnswer = $(answer).val();
+
+        // SELECT * WHERE "_id" = idQuestion
+        var rightAnswer = Questions.findOne({ "_id": idQuestion});
+
+console.log(userAnswer);
+console.log(rightAnswer.correctAnswer);
+
+        if (rightAnswer.correctAnswer == userAnswer) {
+            console.log("Pregunta Acertada");
+        }
+        else {
+            console.log("Pregunta Fallada");
+        }
+        //TODO - Insert estado de la pregunta en el usuario
+        Meteor.call('questions.checkAnswer', answer, idQuestion);
+    }
+});
