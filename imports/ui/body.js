aa0fbe45038428a7a4765da02a1cefeb3d741f90
Jorgesanel <jorgesantosneila@hotmail.com>
2017-08-22 20:51:46 +0200

    Added questions from Balance and made logic to switch between questions depending on user's answer

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,4 +1,6 @@
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session'
+
 import { menu } from './structure/menu.js';
 import { footer } from './structure/footer.html';
 import { userLogIn } from './userLogIn.js'
@@ -9,28 +11,53 @@ import './body.css';
 
 Template.body.helpers({
     Questions() {
-        return Questions.find()
+        var currentQuestion = Session.get('currentQuestion') || 1;
+        Session.set('currentQuestion', currentQuestion);
+
+        return Questions.findOne({"_id" : String(currentQuestion)});
     },
 })
 
-Template.question.events({
+var jokerFailed = false;
+Template.body.events({
     'click :button': function (event, template) {
-        var idQuestion = 1;
+
+        var idQuestion = Session.get('currentQuestion');
         var answer = template.find('input:radio[name=answer]:checked');
         var userAnswer = $(answer).val();
 
         // SELECT * WHERE "_id" = idQuestion
-        var rightAnswer = Questions.findOne({ "_id": idQuestion.ToString()});
+        var rightAnswer = Questions.findOne({ "_id": String(idQuestion)});
 
-console.log(userAnswer);
-console.log(rightAnswer.correctAnswer);
+        var nextQuestion = 0;
+        // Set currentQuestion to next one
+        if(rightAnswer.location.circuit == "Balance"){
+                nextQuestion = idQuestion + 2;
+        }else if(rightAnswer.isUnique == true){
+            if(jokerFailed)
+                nextQuestion = idQuestion + 2;
+            else
+                nextQuestion = idQuestion + 1;
+        }else{
+            if(jokerFailed)
+                nextQuestion = idQuestion + 1;
+            else
+                nextQuestion = idQuestion + 2;
+        }
 
         if (rightAnswer.correctAnswer == userAnswer) {
-            console.log("Pregunta Acertada");
+            // Right Answer
+
+            //TODO - Insert into database the solution
+        }else {
+            // Wrong Answer
+            if(rightAnswer.isJoker == true){
+                jokerFailed = true;
+                nextQuestion = 2;
+            }
         }
-        else {
-            console.log("Pregunta Fallada");
-        }
+        Session.set('currentQuestion', nextQuestion);        
+
     //TODO - Insert estado de la pregunta en el usuario
     // ../api/questions.js - La idea es que tire de este fichero
         //Meteor.call('questions.checkAnswer', answer, idQuestion);
